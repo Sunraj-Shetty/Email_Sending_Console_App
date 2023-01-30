754ac68a132cc41fcd810ad143e4b1614bb5296b
Sunraj-Shetty <sunraj.martha@raasoft.com>
2023-01-30 15:47:20 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("raasoft.developer@gmail.com");
+                mail.To.Add("sunrajshetty17@gmail.com");
+                mail.Subject = "Test Email";
+                mail.Body = "This is a test email sent from .NET program.";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("raasoft.developer@gmail.com", "fqpdvbbrwsdhtosh");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
